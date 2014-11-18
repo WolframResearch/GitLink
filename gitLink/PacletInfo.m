c7906b801e4cacd369c13467f4d239eee09f5848
Lou D'Andria <lou@wolfram.com>
2014-11-18 10:04:13 -0600

    Initial commit of the PacletInfo.m file.

---
+++ gitLink/PacletInfo.m
@@ -0,0 +1,28 @@
+
+Paclet[
+    Name -> "gitLink",
+    Version -> "0.0.1",
+    MathematicaVersion -> "10+",
+    Root -> ".",
+    Internal -> True,
+    Extensions -> 
+        {
+            {"Kernel", Root -> ".", Context -> "gitLink`"},
+
+            {"Documentation", Language -> "English"},
+
+			{"Resource", SystemID ->"Linux-x86-64", Resources -> {
+				{"gitLink.so", "LibraryResources/Linux-x86-64/gitLink.so"}
+			}},
+
+			{"Resource", SystemID ->"MacOSX-x86-64", Resources -> {
+				{"gitLink.dylib", "LibraryResources/MacOSX-x86-64/gitLink.dylib"}
+			}},
+
+			{"Resource", SystemID ->"Windows-x86-64", Resources -> {
+				{"gitLink.dll", "LibraryResources/Windows-x86-64/gitLink.dll"},
+				{"gitLink.exp", "LibraryResources/Windows-x86-64/gitLink.exp"},
+				{"gitLink.lib", "LibraryResources/Windows-x86-64/gitLink.lib"}
+			}}
+        }
+]
