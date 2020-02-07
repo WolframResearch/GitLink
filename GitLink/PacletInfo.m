771bba0a29c6ef0266b0ceec459f0047a4eb532c
Lou D'Andria <lou@wolfram.com>
2020-02-07 13:40:07 -0600

    There's no reason to hide this paclet from PacletFind.

--- GitLink/PacletInfo.m
+++ GitLink/PacletInfo.m
@@ -5,7 +5,6 @@ Paclet[
     Version -> "2100.0",
     MathematicaVersion -> "11.1+",
     Root -> ".",
-    Internal -> True,
     Extensions -> 
         {
             {"Kernel", Root -> ".", Context -> "GitLink`"},
--- GitLink/PacletInfoTemplate.m
+++ GitLink/PacletInfoTemplate.m
@@ -5,7 +5,6 @@ Paclet[
     Version -> "`version`",
     MathematicaVersion -> "11.1+",
     Root -> ".",
-    Internal -> True,
     Extensions -> 
         {
             {"Kernel", Root -> ".", Context -> "GitLink`"},
